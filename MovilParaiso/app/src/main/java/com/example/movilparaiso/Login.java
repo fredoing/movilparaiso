204b53b101d3b25f2e0f9162f47d048a1aa4fc4d
Alfredo Campos <joscaes12@gmail.com>
2019-10-26 00:46:02 -0600

    Added new login layout and features

--- MovilParaiso/app/src/main/java/com/example/movilparaiso/Login.java
+++ MovilParaiso/app/src/main/java/com/example/movilparaiso/Login.java
@@ -2,7 +2,12 @@ package com.example.movilparaiso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +15,27 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button btn_login = findViewById(R.id.btnLogin);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                authenticate();
+            }
+        });
+
+    }
+
+    public void authenticate() {
+        EditText editUser = findViewById(R.id.username);
+        EditText editpass = findViewById(R.id.password);
+        String user = editUser.getText().toString();
+        String pass = editpass.getText().toString();
+
+        openMenu();
+    }
+
+    public void openMenu() {
+
     }
 }

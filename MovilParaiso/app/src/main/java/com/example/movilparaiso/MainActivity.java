4dedeb4b63161e0a7b48719bae22d515d672a0c5
Alfredo Campos <joscaes12@gmail.com>
2019-10-25 23:55:02 -0600

    Set main to open login activity

--- MovilParaiso/app/src/main/java/com/example/movilparaiso/MainActivity.java
+++ MovilParaiso/app/src/main/java/com/example/movilparaiso/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.movilparaiso;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_login = findViewById(R.id.btnLogin);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLoginScreen();
+            }
+        });
+    }
+
+    public void openLoginScreen() {
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
     }
 }
